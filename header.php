6810afb52f9e12418638049863bf1e068a2f8582
ibrahim729 <83731014+ibrahim729@users.noreply.github.com>
2021-05-05 13:53:30 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>News</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!-- Custom stlylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- HEADER -->
+<div id="header">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <!-- LOGO -->
+            <div class=" col-md-offset-4 col-md-4">
+                <a href="index.php" id="logo"><img src="images/news.jpg"></a>
+            </div>
+            <!-- /LOGO -->
+        </div>
+    </div>
+</div>
+<!-- /HEADER -->
+<!-- Menu Bar -->
+<div id="menu-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class='menu'>
+                <?php
+                 include 'config.php';
+                 $sql="SELECT * FROM category where post > 0";
+                 $result=mysqli_query($conn,$sql) or die("Query failed.");
+                 if(mysqli_num_rows($result)>0){
+                     while($row=mysqli_fetch_assoc($result)){
+                         
+                         echo "<li><a  href='category.php>{$row['category_name']}</a></li>";
+                     }
+                     echo "</ul>";
+                 }
+                ?>
+                    
+                    
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Menu Bar -->
